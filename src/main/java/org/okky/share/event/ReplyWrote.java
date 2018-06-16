6b37725c29857e0041adf116a91e49d404e2af27
hyunsu <coding8282@gmail.com>
2018-06-17 01:52:21 +0900

    샘플 추가

--- src/main/java/org/okky/share/event/ReplyWrote.java
+++ src/main/java/org/okky/share/event/ReplyWrote.java
@@ -2,7 +2,9 @@ package org.okky.share.event;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.okky.share.JsonUtil;
 
+import static java.lang.System.currentTimeMillis;
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
@@ -27,5 +29,22 @@ public class ReplyWrote extends DomainEvent {
         this.repliedOn = repliedOn;
         this.updatedOn = updatedOn;
         this.acceptedOn = acceptedOn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JsonUtil.toPrettyJson(sample()));
+    }
+
+    public static ReplyWrote sample() {
+        String id = "r-1";
+        String articleId = "a-1";
+        String body = "내용";
+        String replierId = "m-1";
+        String replierName = "coding8282";
+        long repliedOn = currentTimeMillis();
+        Long updatedOn = null;
+        Long acceptedOn = null;
+        ReplyWrote event = new ReplyWrote(id, articleId, body, replierId, replierName, repliedOn, updatedOn, acceptedOn);
+        return event;
     }
 }
